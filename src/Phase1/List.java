ed708c5b4b65051505a8998d7c892837e071a3d5
Sam Dietrich <sam.dtrik@hotmail>
2016-10-26 13:25:34 -0600

    Fixed tail.next bug

--- src/Phase1/List.java
+++ src/Phase1/List.java
@@ -14,6 +14,7 @@ public class List {
 			tail.next=le;
 		}
 		tail=le;
+		tail.next=null; //nothing is next of tail 
 		n++;
 	}
 	
@@ -34,7 +35,7 @@ public class List {
 		else{
 			ListElement returnElement = new ListElement();
 			returnElement = head;
-			for(int i=0;i<index;i++){
+			for(int i=1;i<index;i++){
 				returnElement = returnElement.next;
 			}
 			return returnElement;
@@ -43,6 +44,35 @@ public class List {
 	
 	public ListElement deleteElement(int index){
 		
+		if (index==0){
+			return new ListElement(); //essentially null value
+		}
+		else if (index==1){
+			head.next = head;	//head is no longer in list
+			return head;
+		}
+		else if (index==n){
+			ListElement returnElement = new ListElement();
+			returnElement = head;
+			for(int i=1;i<index-1;i++){
+				returnElement = returnElement.next;
+			}
+			returnElement = tail; 		//tail is no longer in list
+			returnElement.next = null;
+			return returnElement;
+		}
+		else if (index>n){
+			return new ListElement(); //essentially null value
+		}
+		else{
+			ListElement returnElement = new ListElement();
+			returnElement = head;
+			for(int i=1;i<index-1;i++){
+				returnElement = returnElement.next;
+			}
+			returnElement.next = returnElement.next.next;		//listelement is now 'jumped' over in list, it no longer exists
+			return returnElement;
+		}
 		
 	}
 	
@@ -52,6 +82,13 @@ public class List {
 	}
 	
 	public void printLinkedListHead(){
+		ListElement printElement = new ListElement();
+		printElement = head;
+		for(int i=1;i<n;i++){
+			System.out.println(printElement);
+			printElement = printElement.next;
+		}
+		System.out.println(printElement); //prints elements which is not printed in the list
 		
 	}
 	
